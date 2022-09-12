c131f21fce006967868ed38f34cae00533af8d94
ValerioScharizza <valerio.schiarizza@live.it>
2022-09-12 14:01:54 +0200

    Done exercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 console.log('JS OK!');
 
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Hello everyone, i\'m using Vue.js!'
+        }
+    }
+)
